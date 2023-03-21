2027797c5978a8bb339b413b09b9de9ef6d0e384
Son <sonnvph20319@fpt.edu.vn>
2023-03-21 21:02:19 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,62 @@
+// npm install express-handlebars
+// npm install express
+// npm install http 
+// npm install -g nodemon
+//npm install --save multer
+
+const express = require('express')
+const app = express()
+const port = 3030
+const bodyParser = require('body-parser')
+const multer = require('multer');
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// SET STORAGE
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads')
+    },
+    filename: function (req, file, cb) {
+        let fileName = file.originalname;
+        console.log('fileName');
+
+        let arr = fileName.split('.');
+        let newFileName = arr[0] + '-' + Date.now + '.'+ arr[1];
+        
+
+        cb(null, newFileName);
+    }
+})
+
+var upload = multer({ storage: storage })
+
+app.post('/uploadfile', upload.single('myFile'), (req, res, next) => {
+    const file = req.file
+    if (!file) {
+        const error = new Error('Please upload a file')
+        error.httpStatusCode = 400
+        return next(error)
+    }
+    res.send(file)
+})
+
+//Uploading multiple files
+app.post('/uploadmultiple', upload.array('myFiles', 12), (req, res, next) => {
+    const files = req.files
+    if (!files) {
+        const error = new Error('Please choose files')
+        error.httpStatusCode = 400
+        return next(error)
+    }
+    res.send(files)
+})
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/upload.html');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+});
